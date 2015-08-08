08a254d5f2a08f7efae03ee6e12bf219463a1de4
d.takeuchi <d.takeuchi@technoplan-inc.co.jp>
2015-08-09 00:27:25 +0900

    他のユーザーの情報が見えないように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :show]
+  before_action :correct_user, only:[:show]
 
   def index
     @users = User.all
@@ -24,11 +25,14 @@ class UsersController < ApplicationController
     end
   end
 
-
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
