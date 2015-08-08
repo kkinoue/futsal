1233ecc952882d450d49fa017a7aa8e2b28ddc8a
d.takeuchi <d.takeuchi@technoplan-inc.co.jp>
2015-08-08 23:25:59 +0900

    refs #11 ラベルや日付の日本語化
    ベタ書きとかも含めて大体日本語にした。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       sign_in user
       redirect_back_or events_path
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'メールアドレスとパスワードが一致しません'
       render 'new'
     end
   end
